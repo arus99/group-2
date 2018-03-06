ed1cbbde5bff108f1b3027858d4cfc4f653a7855
Michael Sargsyan <michael.sargs1an@gmail.com>
2018-03-06 20:56:41 +0400

    FIRST COMMIT

--- lesson1/arus/lessons/lessons1/scripts/scripts2.sh
+++ lesson1/arus/lessons/lessons1/scripts/scripts2.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
-cd ../output/U1/
-echo "a.txt" > a.txt
-echo "U1:a.txt"
-cat a.txt
+`#!/bin/bash
+cd /workspace/arus/lessons1/output/U1
+"echo U1:a.txt: $(cat a.txt)"
 cd U2/U3/
-echo "a.txt" > a.txt
-echo "U3:a.txt"
-cat a.txt
-echo "b.txt" > b.txt
-echo "U3:b.txt"
-cat b.txt
+"echo U3:a.txt: $(cat a.txt)"
+"echo U3:b.txt: $(cat b.txt)"
+
